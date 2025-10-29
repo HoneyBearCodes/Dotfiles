c39207ede799d91d8957f7c50d9b7e0d922256ac
Shashank Singh <96770927+HoneyBearCodes@users.noreply.github.com>
2025-10-29 18:21:28 +0530

    [🌟] - Update lazyvim python lsp configs

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -60,12 +60,14 @@ return {
           },
         },
       },
+
       setup = {
         [ruff] = function()
-          LazyVim.lsp.on_attach(function(client, _)
+          require("snacks.util.lsp").on({ name = ruff }, function(event)
+            local client = event.client
             -- Disable hover in favor of Pyright
             client.server_capabilities.hoverProvider = false
-          end, ruff)
+          end)
         end,
       },
     },
